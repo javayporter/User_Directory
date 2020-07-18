35714c74563f8ec8d86eafcb3626669fc6c9b628
javayporter <javayporter@gmail.com>
2020-07-18 16:13:15 -0400

    moved files from non git rrepo to current repo

---
+++ src/components/DataBody.js
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import "../styles/DataBody.css";
+import DataAreaContext from "../utils/DataAreaContext";
+
+const DataBody = () => {
+  const context = useContext(DataAreaContext);
+
+  return (
+    <tbody>
+      {context.developerState.filteredUsers[0] !== undefined && context.developerState.filteredUsers[0].name !== undefined ? (
+        context.developerState.filteredUsers.map(({ login, name, phone, email }) => {
+          return (
+            <tr key={login.uuid}>
+          
+            
+              <td data-th="Name" className="name-cell align-middle">
+                {name.first} {name.last}
+              </td>
+              <td data-th="Phone" className="align-middle">
+                {phone}
+              </td>
+              <td data-th="Email" className="align-middle">
+                <a href={"mailto:" + email} target="__blank">
+                  {email}
+                </a>
+              </td>
+            
+            </tr>
+          );
+        })
+      ) : (
+        <></>
+      )}
+    </tbody>
+  );
+}
+
+export default DataBody;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../styles/Header.css";
+
+function Header () {
+    return (
+      <div className="header">
+        <h1>Welcome to the User Director!</h1>
+        <p>This app allows you to search or filter by user name. Get Started!</p>
+      </div>
+    )
+  }
+
+export default Header;
